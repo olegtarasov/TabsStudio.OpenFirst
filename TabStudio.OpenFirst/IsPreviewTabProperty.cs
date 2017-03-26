5273cf1ac3a15ab42eef3f5bdcbd5eb018bdc666
Oleg Tarasov <oleg.v.tarasov@gmail.com>
2017-03-26 10:45:25 +0300

    Initial commit

---
+++ TabStudio.OpenFirst/IsPreviewTabProperty.cs
@@ -0,0 +1,16 @@
+﻿namespace AddinsCommon
+{
+    class IsPreviewTabProperty
+    {
+        public bool IsPreviewTab(System.Windows.UIElement tab)
+        {
+            if (isPreviewTabDescriptor == null)
+                isPreviewTabDescriptor = System.ComponentModel.DependencyPropertyDescriptor.FromName(
+                    "IsPreviewTab", tab.GetType(), tab.GetType());
+
+            return (bool)isPreviewTabDescriptor.GetValue(tab);
+        }
+
+        private System.ComponentModel.DependencyPropertyDescriptor isPreviewTabDescriptor;
+    }
+}
---
+++ TabStudio.OpenFirst/OpenFirstExtension.cs
@@ -0,0 +1,37 @@
+﻿using AddinsCommon;
+using EnvDTE80;
+using TabsStudioExt;
+
+namespace TabStudio.OpenFirst
+{
+    public class OpenFirstExtension : ITabsStudioAddin2
+    {
+        private readonly IsPreviewTabProperty _isPreviewTab = new IsPreviewTabProperty();
+
+        private ITabsStudioEngine _engine;
+
+
+        public void OnConnection(ITabsStudioEngine engine, DTE2 dte, IVsPackage package)
+        {
+            _engine = engine;
+            _engine.TabCreated += EngineOnTabCreated;
+        }
+
+        private void EngineOnTabCreated(object sender, TabEventArgs e)
+        {
+            if (_isPreviewTab.IsPreviewTab(e.Tab.TabItem))
+                return;
+
+            var container = e.Tabs.TabPanel2.Children;
+            if (ReferenceEquals(container[0], e.Tab.TabItem))
+                return;
+
+            container.Remove(e.Tab.TabItem);
+            container.Insert(0, e.Tab.TabItem);
+        }
+
+        public void OnDisconnection()
+        {
+        }
+    }
+}
